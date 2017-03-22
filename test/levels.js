690c65b6ac5f9e17e2c0b7efd8a046fc6e45f6b6
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-03-22 17:43:14 +0100

    Add one more test function call

--- test/levels.js
+++ test/levels.js
@@ -4,3 +4,12 @@ const my_log = new log("api");
 
 my_log.error("aiuto");
 my_log.debug("aiuto");
+
+
+var r = {};
+r.items = {};
+r.items.length = 3;
+r.took = 45;
+r.errors = "srerewe";
+
+my_log.info("ES => " + r.items.length + " indexed, took " + r.took + "ms, errors: " + r.errors);
